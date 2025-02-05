25e657395e054d0894f960eb84dc59d442f5635b
Abhinav <abhinavhooda223@gmail.com>
2025-02-05 14:57:16 +0530

    basic function

--- bot.js
+++ bot.js
@@ -1,19 +1,34 @@
-require("dotenv").config();
-const TelegramBot = require("node-telegram-bot-api");
-const { startCommand } = require("./commands/start");
-const { logRunCommand } = require("./commands/logrun");
-const { viewStatsCommand } = require('./commands/viewStats');
-const { sendWeeklyStats } = require('./commands/weeklyStats');
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+// setup dotenv
+import dotenv from "dotenv";
+dotenv.config();
 
-// Load bot token from .env file
-const token = process.env.BOT_TOKEN;
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start(async (ctx) => {
+    const from = ctx.update.message.from;
+    ctx.reply(`hi ${from.first_name} welcome to stride sense bot.
+        Enter /add_Run to add your runs.
+        Enter /view_Stats to view your weekly stats.
+        Enter /total_Distance to view your weekly stats.
+        `)
+  });
 
-// Create bot instance
-const bot = new TelegramBot(token, { polling: true });
-console.log("running");
-// call commands
-startCommand(bot);
-logRunCommand(bot);
-viewStatsCommand(bot);
-sendWeeklyStats(bot);
-bot.on("polling_error", console.log);
+// Command to add run 
+bot.command('add_Run', (ctx) => {
+    ctx.reply('Enter Distance you ran?')
+})
+
+// Command to view stats
+
+// Command to view Total Distance
+
+bot.on(message('text'), async (ctx) => {
+  console.log(ctx.message);
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
